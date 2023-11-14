32dc39d959b9cc4256f6d8a2a45b9b7b4e4d762b
Mohammad Rizky Maulidhan <61397800+mrizkymaulidhan@users.noreply.github.com>
2023-11-14 22:26:24 +0700

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,53 +3,36 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-def scrape_news(url, headers):
-    response = requests.get(url, headers=headers)
-    soup = BeautifulSoup(response.text, 'html.parser')
+news_data = []
+data_json = {}
+filename = 'scraping_result.json'
 
-    if response.status_code == 200:
-        populer = soup.find('h3', class_='title6', string='Terpopuler')
+url = 'https://www.pikiran-rakyat.com/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
+}
+
+response = requests.get(url, headers=headers)
+soup = BeautifulSoup(response.text,'html.parser')
+
+if response.status_code == 200:
+    populer = soup.find('h3', class_='title6', string='Terpopuler')
+    populer = populer.find_next('div', class_='most__wrap')        
+    populer = populer.find_all('a')
         
-        if populer:
-            obj = populer.find_next('div', class_='most__wrap')
-            obj = obj.find_all('a')
-
-            for item in obj:
-                data_json['link'] = item.get('href')
-                data_json['title'] = item.find('h2').text
-                data_json['scraping_time'] = time.asctime(time.localtime(time.time()))
-                news_data.append(dict(data_json))
-                
-            return news_data
-
-        else:
-            print(f"Elemen 'Terpopuler' tidak ditemukan.")
-            return None
-
-    else:
-        print(f'Request gagal (Status code: {response.status_code}) !!!')
-        return None
-
-def save_to_json(data, filename):
-    if data:
-        with open(filename, "w") as write_file:
-            json.dump(data, write_file, indent=1)
-        print(f'Data berhasil disimpan ke {filename}')
-        
-    else:
-        print('Tidak ada data yang disimpan.')
-
-if __name__ == "__main__":
-    news_data = []
-    data_json = {
-        'link': '',
-        'title': '',
-        'scraping_time': ''
-    }
-    url = 'https://www.pikiran-rakyat.com/'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36'
-    }
+    for item in populer:
+        data_json['url'] = item.get('href')
+        data_json['title'] = item.find('h2').text
+        data_json['scraping_time'] = time.asctime(time.localtime(time.time()))  
+        news_data.append(dict(data_json))
     
-    scraped_data = scrape_news(url, headers)
-    save_to_json(scraped_data, "scraping_result.json")
+else:
+    print(f'Request gagal (Status code: {response.status_code}) !!!')
+
+if news_data:
+    with open (filename, "w") as write_file:
+        json.dump(news_data, write_file, indent=1)
+    print(f'Data berhasil disimpan ke dalam file {filename}!')
+
+else:
+    print(f'Data tidak berhasil disimpan!')
